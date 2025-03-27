ceb914bb1d88db932f00d1cae2bdce7760b4586c
Jake Soenneker <jake@soenneker.com>
2025-03-26 20:28:04 -0500

    cleanup

--- src/IdPartitionPair.cs
+++ src/IdPartitionPair.cs
@@ -1,4 +1,3 @@
-using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 
@@ -8,16 +7,13 @@ namespace Soenneker.Dtos.IdPartitionPair;
 /// A minimal Record type with an Id (string), PartitionKey (string), and maximum JSON compatibility. <para/>
 /// Properties are non-nullable. Lowercase properties when (de)serialized.
 /// </summary>
-[DataContract]
 public record IdPartitionPair
 {
-    [DataMember(Name = "id")]
     [JsonPropertyName("id")]
     [JsonProperty("id")]
-    public string Id { get; set; } = default!;
+    public string Id { get; set; } = null!;
 
-    [DataMember(Name = "partitionKey")]
     [JsonPropertyName("partitionKey")]
     [JsonProperty("partitionKey")]
-    public string PartitionKey { get; set; } = default!;
+    public string PartitionKey { get; set; } = null!;
 }
